018ac88c4e94bd5c116c4769bf72a4cf51c02fc8
artahaam <alireza.thm03@gmail.com>
2026-08-09 20:41:58 +0330

    feat: fetch and store product data from product APIs page by page

--- crawl_categories_pages.py
+++ crawl_categories_pages.py
@@ -26,13 +26,23 @@ def find_all_keys(obj, target_key):
 with open("clothes_category.csv", "r", encoding="utf-8") as csv_file:
 
     reader = csv.DictReader(csv_file)
-
-    print(csv_file.name + " opened")
+    print("Started", datetime.now().strftime("%H:%M:%S"))
+    print("Reading " + csv_file.name)
 
     for row in reader:
 
         cat_id = row["id"]
-        base_page = requests.get(f"https://api.digikala.com/discovery/api/v2/categories/{cat_id}/products").json()
+
+        try:
+            
+            base_page = requests.get(f"https://api.digikala.com/discovery/api/v2/categories/{cat_id}/products/").json()
+
+        except requests.exceptions.ProxyError or requests.exceptions.SSLError as e:
+
+            print(e)
+            print("Aborted")
+            break
+
         pager = find_all_keys(base_page, "pager")[0]
 
         current_page = pager["current_page"]
@@ -42,24 +52,69 @@ with open("clothes_category.csv", "r", encoding="utf-8") as csv_file:
 
         approximate_page_numbers = total_items // 20 + 1
 
+        print("="*20)
         print("Category id: " + cat_id)
         print("Approximate page numbers: " + str(approximate_page_numbers))
 
         for pn in range(1, approximate_page_numbers + 1):
 
+            print(f"Current page: {pn}")
+            
             if total_slots != 0:
-                data = requests.get(f"https://api.digikala.com/discovery/api/v2/categories/{cat_id}/products?page={pn}").json()
 
-                category = cat_id
-                timestamp = datetime.now().strftime("%Y-%m-%d")
-                directory = os.path.join("category_page_json", category, timestamp)
-                os.makedirs(directory, exist_ok=True)
-                
-                with open(f"{directory}/page_{pn}.json", "w", encoding="utf-8") as json_file:
-                    json.dump(data, json_file, indent=4, ensure_ascii=False)
-                json_file.close()
+                try:
+                    data = requests.get(f"https://api.digikala.com/discovery/api/v2/categories/{cat_id}/products?page={pn}").json()
 
-                print('Saved at ' + f"{directory}/page_{pn}.json")
+
+                    category = cat_id
+                    timestamp = datetime.now().strftime("%Y-%m-%d")
+                    directory = os.path.join("category_page_json", category, timestamp)
+                    os.makedirs(directory, exist_ok=True)
+                    
+                    with open(f"{directory}/page_{pn}.json", "w", encoding="utf-8") as json_file:
+                        json.dump(data, json_file, indent=4, ensure_ascii=False)
+                    json_file.close()
+
+
+                    widget = find_all_keys(data, "widgets")[1]
+                    
+                    items = find_all_keys(widget, "data")
+
+                    print("*** Started to store each product page ***")
+                                        
+                    for item in items:
+
+                        print("*"*20)
+                        try:
+                            id = item["id"]
+                        except KeyError:
+                            pass
+
+                        url = f"https://api.digikala.com/v2/product/{id}/"
+
+                        print(f"Trying to save {id}" + f" From {url}")
+
+                        try:
+                            product = requests.get(url).json()
+                        except requests.exceptions.ProxyError as e:
+                            print(e)
+                            print("Aborted due to connection")
+                            break
+
+                        directory = os.path.join("products", category)
+                        os.makedirs(directory, exist_ok=True)
+
+                        with open(f"{directory}/{id}.json", "w", encoding="utf-8") as json_file:
+                            json.dump(product, json_file, indent=4, ensure_ascii=False)
+                        json_file.close()
+
+                        print("Product saved at " +f"{directory}/{id}.json", "---", datetime.now().strftime("%H:%M:%S"))
+                        print("*"*20 + "\n\n")
+
+                    print('Saved at ' + f"{directory}/page_{pn}.json", "---", datetime.now().strftime("%Y-%m-%d"))
+
+                except:
+                    continue
 
             else:
                 break
