f257545697d632e7b1202ed5e87690dbb53fc69b
artahaam <alireza.thm03@gmail.com>
2026-08-10 21:01:34 +0330

    fix: set the currect log directory

--- crawl_categories_pages.py
+++ crawl_categories_pages.py
@@ -6,21 +6,7 @@ from pathlib import Path
 import logging
 
 
-logger = logging.getLogger('crawler')
-logger.setLevel(logging.DEBUG)  
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-
-file_handler = logging.FileHandler('crawler.log')
-file_handler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
 
 
 def find_all_keys(obj, target_key) -> list:
@@ -49,6 +35,28 @@ LOG_DIR = BASE_DIR / "logs"
 
 for directory in (RAW_DIR, CATEGORY_DIR, CHECKPOINT_DIR, LOG_DIR):
     directory.mkdir(parents=True, exist_ok=True)
+
+
+
+
+logger = logging.getLogger('crawler')
+logger.setLevel(logging.DEBUG)  
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler(LOG_DIR / 'crawler.log')
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+
 
 logger.debug("Directories set up finished.")
 
