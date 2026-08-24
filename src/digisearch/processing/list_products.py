97164e62b8da9b9cad31c4fe050ad409f9d2f13e
artahaam <alireza.thm03@gmail.com>
2026-08-24 13:36:01 +0330

    feat: generate a list of all crawled products with their details.json, comments.json and questions.json files + category
    outputing a single file "data/processed/product_list.json" containing all the products and related files makes
    data cleaning steps eazier and cleaner.

--- src/digisearch/processing/list_products.py
+++ src/digisearch/processing/list_products.py
@@ -11,28 +11,33 @@ for cw in catwalk:
     splitted_dirpath = dirpath.split('/')
 
     if 'category' in splitted_dirpath[-1]:
-        category = cw[1][0]
-        CAT_DIR = RAW_DIR / 'category' / category
-        PRO_DIR = CAT_DIR / 'product'        
-        prowalk = walk(PRO_DIR)
-        for pw in prowalk:
-            dirpath = pw[0]
-            splitted_dirpath = dirpath.split('/')
-            if 'product' in splitted_dirpath:
-                product_id = splitted_dirpath[-1]
-                SUB_PRO_DIR = PRO_DIR / str(product_id)
-                try:
-                    questions_path = SUB_PRO_DIR / f'{pw[2][0]}'
-                    details_path = SUB_PRO_DIR / f'{pw[2][1]}'
-                    comments_path = SUB_PRO_DIR / f'{pw[2][2]}'
+        categories = cw[1]
+        for cat in categories:    
+            category = cat
+            CAT_DIR = RAW_DIR / 'category' / category
+            PRO_DIR = CAT_DIR / 'product'        
+            prowalk = walk(PRO_DIR)
+            for pw in prowalk:
+                dirpath = pw[0]
+                splitted_dirpath = dirpath.split('/')
+                if 'product' in splitted_dirpath:
+                    product_id = splitted_dirpath[-1]
+                    SUB_PRO_DIR = PRO_DIR / str(product_id)
+                    try:
+                        questions_path = SUB_PRO_DIR / f'{pw[2][0]}'
+                        details_path = SUB_PRO_DIR / f'{pw[2][1]}'
+                        comments_path = SUB_PRO_DIR / f'{pw[2][2]}'
 
-                    products.append({product_id: {
-                        'questions_path' : str(questions_path),
-                        'details_path': str(details_path),
-                        'comments_path': str(comments_path),
-                    }})
-                except:
-                    continue
+                        products.append({
+                            product_id: {
+                            'questions_path' : str(questions_path),
+                            'details_path': str(details_path),
+                            'comments_path': str(comments_path),
+                        },
+                        'category': cat,
+                        })
+                    except:
+                        continue
 
 with open(PROCESSED_DIR / 'product_list.json', 'w', encoding='utf-8') as file:
     json.dump(products, file,  indent=4)
